d63389994b25cf490dc84816534b05716d435e71
Secant <zzwu@zju.edu.cn>
2019-09-10 18:29:49 +0800

    remove main.py

--- main.py
+++
@@ -1,6 +0,0 @@
-from bdsupreader import BDSupReader
-
-if __name__ == '__main__':
-    x = BDSupReader('./TestSupSet/test_01.sup')
-    for pic in x.iterSubPictures():
-        print(f'timestamp: {pic.startTimeStr} ~ {pic.endTimeStr} | duration: {pic.durationStr} | max alpha: {pic.maxAlpha}')
