0730bcd691863ddfeb97a75ca3476dfe07da4c65
Secant <zzwu@zju.edu.cn>
2019-09-11 21:51:11 +0800

    change data structure from array to linked list for better insertion support. add more examples.

--- examples/print_time_stamps.py
+++ examples/print_time_stamps.py
@@ -4,6 +4,6 @@ sys.path.append('../src')
 from bdsupreader import BDSupReader
 
 if __name__ == '__main__':  
-    x = BDSupReader('../dataset/test_01.sup')
-    for pic in x.iterSubPictures():
+    x = BDSupReader('../dataset/test_02.sup')
+    for pic in x.subPictures:
         print(f'timestamp: {pic.startTimeStr} ~ {pic.endTimeStr} | duration: {pic.durationStr} | max alpha: {pic.maxAlpha}')
