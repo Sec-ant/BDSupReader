7e420124129a84ac624f1d664a5cada5367b0881
Secant <zzwu@zju.edu.cn>
2019-09-12 11:08:16 +0800

    remove example files, fix a minor bug

--- examples/print_time_stamps.py
+++
@@ -1,9 +0,0 @@
-import sys
-sys.path.append('../src')
-
-from bdsupreader import BDSupReader
-
-if __name__ == '__main__':  
-    x = BDSupReader('../dataset/test_02.sup')
-    for pic in x.subPictures:
-        print(f'timestamp: {pic.startTimeStr} ~ {pic.endTimeStr} | duration: {pic.durationStr} | max alpha: {pic.maxAlpha}')
